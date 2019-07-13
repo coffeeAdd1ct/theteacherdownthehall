1240bfaa983343bc526eddad060e246f25861401
Kevin Billeaud <billeaudk@gmail.com>
2019-07-13 11:05:29 -0600

    title

--- head.php
+++ head.php
@@ -1,7 +1,8 @@
+<title>The Teacher Down The Hall</title>
 <link rel="stylesheet" type="text/css" href="index.css" />
 <link rel="icon" href="images/logo.png" type="image" />
 <script src="js/main.js" charset="utf-8"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta property="og:title" content="The Teacher Down the Hall">
+<meta property="og:title" content="The Teacher Down The Hall">
 <meta property="og:url" content="http://theteacherdownthehall.com/index.html">
 <meta property="og:site_name" content="The Teacher Down The Hall">
