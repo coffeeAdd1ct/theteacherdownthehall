aacc69f850d45c21c3d26513103811dd55ddc990
Kevin Billeaud <billeaudk@gmail.com>
2019-07-12 22:52:33 -0600

    under_construction.php

--- contact.php
+++ contact.php
@@ -10,12 +10,7 @@
     <?php include './menu.php';?>
   </div>
   <div class="content">
-    <div class="section">
-      <img src="images/1543873.jpg" />
-      <div class="section_title">This site is under construction.</div>
-      <div class="section_text">Check Back Soon!</div>
-      <div class="section_text"><a href="https://www.teacherspayteachers.com/Store/The-Teacher-Down-The-Hall">In the mean time, check out my TPT resources.</a></div>
-    </div>
+    <?php include './under_construction.php';?>
     <?php include './form.php';?>
   </div>
 </body>
