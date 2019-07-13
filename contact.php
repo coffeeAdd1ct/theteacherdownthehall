aac10a4a548ff019db276721f04320b98a428b0d
Kevin Billeaud <billeaudk@gmail.com>
2019-07-12 23:09:57 -0600

    minimization of head scripts

--- contact.php
+++ contact.php
@@ -1,10 +1,6 @@
 <html>
 <head>
-  <link rel="stylesheet" href="index.css" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta property="og:title" content="The Teacher Down the Hall">
-  <meta property="og:url" content="http://theteacherdownthehall.com/index.html">
-  <meta property="og:site_name" content="theteacherdownthehall.com">
+  <?php include './head.php';?>
 </head>
 <body>
   <div class="header">
