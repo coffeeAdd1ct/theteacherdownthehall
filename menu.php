6fde3c71ce66aaa2b99cd35091fda8559ff6830e
Kevin Billeaud <billeaudk@gmail.com>
2019-07-12 21:04:59 -0600

    initial commit

---
+++ menu.php
@@ -0,0 +1,6 @@
+<div class="menu">
+  <a href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="products.php">Products</a>
+  <a href="contact.php">Contact</a>
+</div>
---
+++ products.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <link rel="stylesheet" href="index.css" />
+  <meta property="og:title" content="The Teacher Down the Hall">
+  <meta property="og:url" content="http://theteacherdownthehall.com/index.html">
+  <meta property="og:site_name" content="theteacherdownthehall.com">
+</head>
+<body>
+  <div class="header">
+    <?php include './menu.php';?>
+  </div>
+  <div class="content">
+    <div class="section">
+      <img src="images/1543873.jpg" />
+      <div class="section_title">This site is under construction.</div>
+      <div class="section_text">Check Back Soon!</div>
+      <div class="section_text"><a href="https://www.teacherspayteachers.com/Store/The-Teacher-Down-The-Hall">In the mean time, check out my TPT resources.</a></div>
+    </div>
+    <?php include './form.php';?>
+  </div>
+</body>
+<html>
