0dbc2f118d38b50635c8b0044e8c5c55aa7d130b
Kevin Billeaud <billeaudk@gmail.com>
2019-07-13 10:58:57 -0600

    logo

--- under_construction.php
+++ under_construction.php
@@ -1,5 +1,5 @@
 <div class="section">
-  <img src="images/1543873.jpg" />
+  <img src="images/logo.png" />
   <div class="section_title">This site is under construction.</div>
   <div class="section_text">Check Back Soon!</div>
   <div class="section_text"><a href="https://www.teacherspayteachers.com/Store/The-Teacher-Down-The-Hall">In the mean time, check out my TPT resources.</a></div>
