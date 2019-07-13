f4e89a76ac9fe4b215a99e13a3cd65c93f9c8cfc
Kevin Billeaud <billeaudk@gmail.com>
2019-07-13 11:00:10 -0600

    meta

--- head.php
+++ head.php
@@ -4,4 +4,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:title" content="The Teacher Down the Hall">
 <meta property="og:url" content="http://theteacherdownthehall.com/index.html">
-<meta property="og:site_name" content="theteacherdownthehall.com">
+<meta property="og:site_name" content="The Teacher Down The Hall">
