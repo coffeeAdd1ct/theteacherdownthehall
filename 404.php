6a09daafd001f2dca9611483a867fcc107796354
Kevin Billeaud <billeaudk@gmail.com>
2019-07-14 15:51:13 -0600

    404 & social media

---
+++ 404.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <?php include './scripts/head.php';?>
+</head>
+<body>
+  <div class="header">
+  </div>
+  <div class="content">
+    <div class="section"><img src="images/logo.png" id="logo"/>
+      <div class="section_text">I'm sorry, it looks like you might have followed a bad link!</div>
+      <div class="section_text">Click this <a href="index.php">HERE to get back Home.</a></div>
+    </div>
+    <?php include './scripts/footer.php';?>
+  </div>
+</body>
+<html>
--- scripts/footer.php
+++ scripts/footer.php
@@ -1,5 +1,9 @@
 <div class="section">
   <div class="footer">
+    <div class="social_media">
+      <a href="https://www.facebook.com/theTeacherDownTheHall/"><img src="images/fb_logo.png" /></a>
+      <a href="https://www.teacherspayteachers.com/Store/The-Teacher-Down-The-Hall"><img src="images/tpt_logo.png" /></a>
+    </div>
     Questions or comments?<br>
     <div>Email us at <a href="mailto:Contact@TheTeacherDownTheHall.com">Contact@TheTeacherDownTheHall.com</a>
   </div>
