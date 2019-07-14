fbfe0e3131d17512a5a18ad90a6b228c1f5446c8
Kevin Billeaud <billeaudk@gmail.com>
2019-07-14 14:08:43 -0600

    tweaks

---
+++ free_download.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <?php include './scripts/head.php';?>
+</head>
+<body>
+  <div class="header">
+    <?php include './scripts/menu.php';?>
+  </div>
+  <div class="content">
+    <div class="section">
+      <img src="images/logo.png" id="logo"/>
+      <div class="section_text">
+        Thanks for your submission!
+        <br>
+        <a href="/images/Slide1.JPG" download>Enjoy your free download!</a>
+      </div>
+    </div>
+    <?php include './scripts/footer.php';?>
+  </div>
+</body>
+<html>
